71ae89b7d782a27591e14da50b284db5a41c15ff
Bob Steers <steers.bob@gmail.com>
2018-12-03 16:51:47 +0100

    Upgrade to python 3.7

--- setup.py
+++ setup.py
@@ -17,12 +17,12 @@ setup(
     keywords='brewing brewpi brewblox embedded plugin service',
     packages=find_packages(exclude=['test']),
     install_requires=[
-        'brewblox-service~=0.13.0',
+        'brewblox-service~=0.14.0',
         'aioinflux~=0.4',
         'dpath~=1.4',
         'aiohttp-sse~=2.0',
         'python-dateutil~=2.7.3',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     setup_requires=['setuptools_scm'],
 )
