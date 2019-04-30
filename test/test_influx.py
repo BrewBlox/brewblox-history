f2031c94705aa4fe1d64a2376800802d27067761
Bob Steers <steers.bob@gmail.com>
2019-04-30 17:15:49 +0200

    add ping endpoint

--- test/test_influx.py
+++ test/test_influx.py
@@ -86,6 +86,10 @@ async def test_query_client(app, client, influx_mock):
     assert data == retval
 
 
+async def test_ping(app, client, influx_mock):
+    await influx.get_client(app).ping()
+
+
 async def test_running_writer(influx_mock, app, client, mocker):
     writer = influx.get_data_writer(app)
 
